7dbb40bea3a66f50f73de174a64772b6cd462e5d
Christian <vargasc475@gmail.com>
2022-10-15 19:37:08 -0400

    Added some changes

--- Week05/Team Activity/js/HikesView.js
+++ Week05/Team Activity/js/HikesView.js
@@ -17,7 +17,7 @@ export default class HikesView {
         // this mehtod will be used to create the list of hikes with less detail: name, image,
         const item = document.createElement('li');
         item.innerHTML = `<h3>${hike.name}</h3>
-                        <img src='./falls.jpg' alt='${hike.imgAlt}'>
+                        <img src='${this.imgBasePath}${hike.imgSrc}' alt='${hike.imgAlt}'>
                         <p>Distance: ${hike.distance}</p>
                         <p>Difficulty: ${hike.difficulty}</p>`
         return item;
@@ -25,5 +25,6 @@ export default class HikesView {
     }
     renderOneHikeFull(hike, parentElement) {
         // this method will be used to one hike full detail... you will need this for the
+        
     }
 }
