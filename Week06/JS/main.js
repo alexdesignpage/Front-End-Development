f33ba0f9271a3f9f5ff2814cde7d8effca7f8366
alexdesignpage <61528847+alexdesignpage@users.noreply.github.com>
2022-10-23 00:47:37 -0400

    c

---
+++ Week06/JS/main.js
@@ -0,0 +1,30 @@
+const links = [
+    {
+        label: "Practice",
+        url: "Practice/index.html"
+    },
+    {
+        label: "Notes",
+        url: "Notes/index.html"
+    },
+   
+    
+    {
+        label: "To-Do Challange",
+        url: "To Do Challange/index.html"
+    }
+];
+
+
+var contents = document.getElementById("contents");
+
+function generateContents() {
+    links.forEach(function (item, index) {
+        console.log(index, item);
+        let listItem = "<div><a href=\"" + item.url + "\" >" + item.label + "</a></div>";
+        console.log(listItem);
+        contents.innerHTML += listItem;
+    })
+}
+
+document.addEventListener("load", generateContents());
