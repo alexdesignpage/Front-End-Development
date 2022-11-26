0d5b54a26015ed8acf58a21e6c8a0b40d21564c9
alexdesignpage <61528847+alexdesignpage@users.noreply.github.com>
2022-11-25 22:31:34 -0500

    week11

---
+++ Week11/JS/main.js
@@ -0,0 +1,26 @@
+const links = [
+    {
+        label: "Reading",
+        url: "Reading/index.html"
+    },
+   
+       
+    {
+        label: "Team Activity",
+        url: "auth/client/week11.html"
+    }
+];
+
+
+var contents = document.getElementById("contents");
+
+function generateContents() {
+    links.forEach(function (item, index) {
+        console.log(index, item);
+        let listItem = "<div><a href=\"" + item.url + "\" >" + item.label + "</a></div>";
+        console.log(listItem);
+        contents.innerHTML += listItem;
+    })
+}
+
+document.addEventListener("load", generateContents());
